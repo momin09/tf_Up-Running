df5d0dc72b06fd2240613995bb5f673773757da3
momin09 <khm0909z@gmail.com>
2023-01-06 19:57:07 +0900

    Create a Script for EC2

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ resource "aws_instance" "example" {
     ami = "ami-0cb1d752d27600adb"
     instance_type = "t2.micro"
 
+    user_data = <<-EOF
+                    #! /bin/bash
+                    echo "Hello, world" > index.html
+                    nohup busybox httpd -f -p 8080 &
+                    EOF
+
     tags = {
         Name = "terraform-example"
     }
