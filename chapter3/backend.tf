b08f107f33080f2fd93572cd59eb09a03ca8a62b
momin09 <khm0909z@gmail.com>
2023-01-09 15:37:47 +0900

    Modify: the Key of s3

--- chapter3/backend.tf
+++ chapter3/backend.tf
@@ -2,11 +2,11 @@ terraform {
     backend "s3" {
         # Change the name before you create
         bucket  = "terraform-up-and-running-momin"
-        key     = "global/s3/terraform.tfstate"
+        key     = "workspaces-example/terraform.tfstate"
         region  = "ap-northeast-2"
 
         # Change the DynamoDB name before you create
-        dynamodb_table  = "terraform-up-and-running-moMin-locks"
+        dynamodb_table  = "terraform-up-and-running-momin-locks"
         encrypt         = true
     }
 }
