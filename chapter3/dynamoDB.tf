ed6f9206d0d639ed4a9410bf4867d6fdcc62d593
momin09 <khm0909z@gmail.com>
2023-01-09 15:11:03 +0900

    Modify: the name of DynamoDB table

--- chapter3/dynamoDB.tf
+++ chapter3/dynamoDB.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "terraform_locks" {
-    name        = "terraform-up-and-running-moMin-locks"
+    name        = "terraform-up-and-running-momin-locks"
     billing_mode = "PAY_PER_REQUEST"
     hash_key = "LockID"
 
