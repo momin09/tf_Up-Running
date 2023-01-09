817fca520f71646061ec9dddbddcc572d5b89637
momin09 <khm0909z@gmail.com>
2023-01-09 13:43:56 +0900

    Create: S3.tf

---
+++ chapter3/s3.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "terraform_state" {
+    bucket = "terraform-up-and-running-moMin"
+
+    # Prevent destroy S3 bucket
+    lifecycle {
+        prevent_destroy = true
+    }
+
+    # Version Managing
+    versioning {
+        enabled = true
+    }
+
+    # Creating a encryption for Server
+    server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+        }
+      }
+    }
+
+}
