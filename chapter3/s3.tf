d7ce98c7067e4a341dc1b80ca47441c949caac04
momin09 <khm0909z@gmail.com>
2023-01-09 14:02:03 +0900

    Modify: the name of S3

--- chapter3/s3.tf
+++ chapter3/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-    bucket = "terraform-up-and-running-moMin"
+    bucket = "terraform-up-and-running-momin"
 
     # Prevent destroy S3 bucket
     lifecycle {
@@ -15,7 +15,7 @@ resource "aws_s3_bucket" "terraform_state" {
     server_side_encryption_configuration {
       rule {
         apply_server_side_encryption_by_default {
-            sse_algorithm = "AES256"
+          sse_algorithm = "AES256"
         }
       }
     }
